60c3390fea2838c179b75c2fc531a30e6af3a496
alex braatz <alexjbraatz@gmail.com>
2021-03-24 13:38:47 -0500

    can console long dynamic inputs from DOM, still need to render to DOM

--- react-actors/src/components/Contact/Contact.jsx
+++ react-actors/src/components/Contact/Contact.jsx
@@ -1,13 +1,43 @@
 import { useState } from 'react';
 
 function Contact(){
+    const [ fname, setFirstName ] = useState( '' );
+    const [ lname, setLastName  ] = useState( '' );
+    const [ impression, setImpression ] = useState( '' );
+
+    let handleFirst = ( event ) => {
+        console.log( 'in handleFirst:', event.target.value );
+        setFirstName( event.target.value);
+    }
+
+    let handleLast = ( event ) => {
+        console.log( 'in handleLast:', event.target.value );
+        setLastName( event.target.value );
+    }
+
+    let handleImpression = ( event ) => {
+        console.log( 'in handleImpression:', event.target.value );
+        setImpression( event.target.value );
+    }
+
+    let userGreeting = ( props ) => {
+        console.log( {fname}, {lname}, {impression} )
+        return <p>{fname} {lname}'s best impression is {impression}!</p>
+    }
+
+    let handleJoin = () => {
+        if ( fname && lname && impression ){
+            userGreeting();
+        }
+    }
+
     return (
         <div>
             <h2>Contact</h2>
-            <input type="text" placeholder="First Name"></input>
-            <input type="text" placeholder="Last Name"></input>
-            <input type="text" placeholder="Best Impression"></input>
-            <button>Join Us!</button>
+            <input type="text" placeholder="First Name" onChange={ handleFirst }></input>
+            <input type="text" placeholder="Last Name" onChange={ handleLast }></input>
+            <input type="text" placeholder="Best Impression" onChange={ handleImpression }></input>
+            <button onClick={ handleJoin }>Join Us!</button>
             <p>Phone: 1-888-WE-REACT</p>
             <p>Email: reactWithUs@ReactActors.com</p>
         </div>
