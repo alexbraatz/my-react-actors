d0fe4180703255b4d87bd360cf9596ec72707bc8
alex braatz <alexjbraatz@gmail.com>
2021-03-24 16:58:34 -0500

    updates to DOM user inputs

--- react-actors/src/components/Contact/Contact.jsx
+++ react-actors/src/components/Contact/Contact.jsx
@@ -4,30 +4,26 @@ function Contact(){
     const [ fname, setFirstName ] = useState( '' );
     const [ lname, setLastName  ] = useState( '' );
     const [ impression, setImpression ] = useState( '' );
+    const [ message, setMessage ] = useState( '' );
 
     let handleFirst = ( event ) => {
-        console.log( 'in handleFirst:', event.target.value );
         setFirstName( event.target.value);
     }
 
     let handleLast = ( event ) => {
-        console.log( 'in handleLast:', event.target.value );
         setLastName( event.target.value );
     }
 
     let handleImpression = ( event ) => {
-        console.log( 'in handleImpression:', event.target.value );
         setImpression( event.target.value );
     }
 
-    let userGreeting = ( props ) => {
-        console.log( {fname}, {lname}, {impression} )
-        return <p>{fname} {lname}'s best impression is {impression}!</p>
-    }
-
-    let handleJoin = () => {
-        if ( fname && lname && impression ){
-            userGreeting();
+    let isInputed = () => {
+    
+        if ( fname && lname && impression ) {
+            console.log( fname, lname, impression );
+            setMessage( `${fname} ${lname}'s best impression is ${impression}`);
+            console.log( message );
         }
     }
 
@@ -37,8 +33,9 @@ function Contact(){
             <input type="text" placeholder="First Name" onChange={ handleFirst }></input>
             <input type="text" placeholder="Last Name" onChange={ handleLast }></input>
             <input type="text" placeholder="Best Impression" onChange={ handleImpression }></input>
-            <button onClick={ handleJoin }>Join Us!</button>
+            <button onClick={ isInputed }>Join Us!</button>
             <p>Phone: 1-888-WE-REACT</p>
+            <p>{message}</p>
             <p>Email: reactWithUs@ReactActors.com</p>
         </div>
     )
