2aa745a6f475f899bffb616f79a86ea1fde8fcef
alex braatz <alexjbraatz@gmail.com>
2021-03-24 10:58:15 -0500

    render input boxes first name, last name, best impression + join us btn

--- react-actors/src/components/Contact/Contact.jsx
+++ react-actors/src/components/Contact/Contact.jsx
@@ -1,7 +1,13 @@
+import { useState } from 'react';
+
 function Contact(){
     return (
         <div>
             <h2>Contact</h2>
+            <input type="text" placeholder="First Name"></input>
+            <input type="text" placeholder="Last Name"></input>
+            <input type="text" placeholder="Best Impression"></input>
+            <button>Join Us!</button>
             <p>Phone: 1-888-WE-REACT</p>
             <p>Email: reactWithUs@ReactActors.com</p>
         </div>
